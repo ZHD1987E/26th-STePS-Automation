81c6c939a0111b3776490a44d329ac7717eae56a
ZHD1987E <zhanghaodong101@outlook.com>
2025-03-26 18:03:38 +0800

    Doing away with video links system

--- scraping.py
+++ scraping.py
@@ -5,12 +5,10 @@ from datetime import datetime
 
 moduleCodes = ["CP3108","CS3203","CS3217","CS3247","CS4240","CS4248","MComp-FYP"]
 f1 = open("project names.md","w", encoding="utf-8")
-f2 = open("project videos.md", "w", encoding="utf-8")
 f3 = open("project-details.json", "w", encoding="utf-8")
 jsonDCT = {}
 for moduleCode in moduleCodes:
     f1.write(f"# {moduleCode} project names\n")
-    f2.write(f"# {moduleCode} project videos\n")
     baseURL = f"https://uvents.nus.edu.sg/event/26th-steps/module/{moduleCode}/project/"
     jsonDCT[moduleCode] = {}
     k = 1
@@ -22,20 +20,17 @@ for moduleCode in moduleCodes:
                 div1 = soup.find('div',class_="text-container")
                 div2 = soup.find('div', class_="section media")
                 div3 = soup.find('div', class_="student-list-wrapper")
+                vlink = ""
                 if div2:
                     youtube_links = div2.find_all('a', href=lambda href: href and ("youtube.com" in href or "youtu.be" in href))
                     if youtube_links:
                         for link in youtube_links:
-                            f2.write(f"{moduleCode}-{k}: {link['href']}\n\n")
-                    else:
-                        f2.write(f"{moduleCode}-{k}: NIL\n\n")
-                else:
-                    f2.write(f"{moduleCode}-{k}: NIL\n\n")
+                            vlink = link['href']
                 projName = div1.find('h3').text.strip()
                 studentList1 = div3.findAll('li')
-                finalStudentList = list(map(lambda e: e.text.strip().title(), studentList1))
+                finalStudentList = set(map(lambda e: e.text.strip().title(), studentList1))
                 f1.write(f"{moduleCode}-{k}: {projName}\n\n")
-                jsonDCT[moduleCode][k] = {'projName': projName, 'nameList': finalStudentList}
+                jsonDCT[moduleCode][k] = {'projCode': f'{moduleCode}-{k}', 'projName': projName, 'nameList': finalStudentList, 'videoLink': vlink}
                 k += 1
             else:
                 break
@@ -44,8 +39,6 @@ for moduleCode in moduleCodes:
             f2.write(f"An error somehow occured. {e}\n")
             break
 f1.write(f"Last updated: {datetime.now()}")
-f2.write(f"Last updated: {datetime.now()}")
 json.dump(jsonDCT, f3, indent = 4)
 f1.close()
-f2.close()
 f3.close()
