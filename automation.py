36166021c8448713b24b87e3a9402ed84b1acf2b
zhd1987e <zhanghaodong101@outlook.com>
2025-03-27 12:50:49 +0800

    Now including autodownload!

--- automation.py
+++ automation.py
@@ -1,5 +1,8 @@
 import requests
 import csv
+from pytubefix import YouTube
+import os
+import ffmpeg
 theJSON = requests.get("https://uvents.nus.edu.sg/api/event/26th-steps/moduleTracks").json()
 f1 = open("26th-steps-data.csv", "w", newline="")
 csvwriter = csv.writer(f1)
@@ -19,5 +22,17 @@ for track in theJSON:
         projectNUMBER = project["refId"]
         f2.write(f"{trackCODE}-{projectNUMBER}: {projectNAME} \n\n")
         csvwriter.writerow([trackCODE, projectNUMBER, projectNAME, projectVIDEOURL, "; ".join(projectMEMBERS), ""])
+        if projectVIDEOURL != "":
+            try:
+                yt = YouTube(projectVIDEOURL) # assuming ALL project videos are YouTube links
+                videoStream = yt.streams.order_by('resolution').desc().first()
+                audioStream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+                videoStream.download(filename="temp1.webm")
+                audioStream.download(filename="temp2.webm")
+                ffmpeg.output(ffmpeg.input("temp1.webm"), ffmpeg.input("temp2.webm"), f"videos/{trackCODE}-{projectNUMBER}.mp4", y = None).run()
+                os.remove("temp1.webm")
+                os.remove("temp2.webm")
+            except:
+                pass
 f1.close()
 f2.close()
