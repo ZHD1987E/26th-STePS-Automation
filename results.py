dfa90c0339d75da16e1ac8ddbde907fecafb304f
zhd1987e <zhanghaodong101@outlook.com>
2025-03-29 02:59:57 +0800

    Decided to simplify matters with JSON input

--- results.py
+++ results.py
@@ -7,28 +7,42 @@ from collections import defaultdict
 # Eventually, this can be used to generate certificates on the fly.
 # The only OTHER option is to manually tag on the data CSV file and then have Google Apps Script manually split the required data.
 # RUN THIS MANUALLY AFTER RESULTS ARE KNOWN!
+# This applies to Undergraudate category only.
 
-theJSON = requests.get("https://uvents.nus.edu.sg/api/event/26th-steps/vote").json()
+theAPIJSON = requests.get("https://uvents.nus.edu.sg/api/event/26th-steps/vote").json()
+theMASTERDATA = open("masterDATA.json", "r", encoding = "utf-8")
+theMASTERDATAJSON = json.load(theMASTERDATA)
 awardsDATA = open("26th-steps-json.json", "r", encoding = "utf-8")
 awardsJSON = json.load(awardsDATA)
 csvDATAFILE = open("26th-steps-awardees.csv", "w", newline="", encoding = "utf-8")
 csvwriter = csv.writer(csvDATAFILE)
-defaultCERTORDER = ["Best Project", "Second Prize", "Third Prize"]
+defaultCERTORDERUNDERGRAD = ["Best Project", "Second Prize", "Third Prize"]
+defaultCERTORDERGRAD = "Honorable Mention"
 defaultCERTNUMBER = 3
 csvwriter.writerow(["Course Names and Heads", "Project Name", "Winner Name", "Award"])
-for course in theJSON:
+for course in theAPIJSON:
     courseCODE = course["module"]
-    awards = defaultCERTORDER
-    maxAwards = defaultCERTNUMBER
+
+    courseNAME = theMASTERDATAJSON[courseCODE]["name"]
+    isGraduate = theMASTERDATAJSON[courseCODE]["isGraduate"]
+    maxAwards = theMASTERDATAJSON[courseCODE]["maxAwards"]
+
+    if isGraduate:
+        awards = [defaultCERTORDERGRAD] * maxAwards
+    else:
+        awards = defaultCERTORDERUNDERGRAD
+    
     courseRESULT = list(course["result"].items())
     courseRESULT.sort(key = lambda x: x[1], reverse = True)
     courseRESULT = list(map(lambda e: f"{courseCODE}-{e[0]}", courseRESULT[:maxAwards]))
+
     for order in range(maxAwards):
         courseAWARDDATA = awardsJSON[courseRESULT[order]]
         winnerNAME = courseAWARDDATA["name"]
         winnerMEMBERS = courseAWARDDATA["members"]
         winnerAWARD = awards[order]
+        
         for member in winnerMEMBERS:
-            csvwriter.writerow([courseCODE, winnerNAME, member, winnerAWARD])
+            csvwriter.writerow([courseNAME, winnerNAME, member, winnerAWARD])
 
 csvDATAFILE.close()
