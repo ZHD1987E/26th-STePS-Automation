decd7750583967d3d0771e81730721440df71eec
zhd1987e <zhanghaodong101@outlook.com>
2025-03-29 00:36:31 +0800

    Update warning

--- results.py
+++ results.py
@@ -3,9 +3,10 @@ import json
 import csv
 
 # Proof of concept only!
-
 # Eventually, this can be used to generate certificates on the fly.
 # The only OTHER option is to manually tag on the data CSV file and then have Google Apps Script manually split the required data.
+# RUN THIS MANUALLY AFTER RESULTS ARE KNOWN!
+
 theJSON = requests.get("https://uvents.nus.edu.sg/api/event/26th-steps/vote").json()
 awardsDATA = open("26th-steps-json.json", "r", encoding = "utf-8")
 awardsJSON = json.load(awardsDATA)
