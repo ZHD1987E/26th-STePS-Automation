0ac10a2c03de62c84da7c9348baf4ca04b343f18
zhd1987e <zhanghaodong101@outlook.com>
2025-03-29 04:07:39 +0800

    Include masterdata entry point

---
+++ convertMASTERDATA.py
@@ -0,0 +1,12 @@
+import csv
+import json
+
+jOUT = {}
+f1 = open('MASTERDATACSV.csv', 'r')
+f2 = open('masterDATA.json', 'w')
+csvreader = csv.reader(f1)
+for thing in list(csvreader):
+    jOUT[thing[0]] = {"name": thing[1], "maxCerts": int(thing[2]), "isGraduate": thing[3] == "TRUE"}
+f1.close()
+f2.write(json.dumps(jOUT, indent=4))
+f2.close()
