3b572c1c6ab326b6ed2c9521bd95116246feb593
zhd1987e <zhanghaodong101@outlook.com>
2025-03-30 18:52:44 +0800

    Modified for .dat files

--- extract.py
+++ extract.py
@@ -10,10 +10,8 @@ theJSON = requests.get("https://uvents.nus.edu.sg/api/event/26th-steps/moduleTra
 
 ## Opening the neccessary files
 f2 = open("26th-steps-projectnames.md", "w", encoding = "utf-8")
-f3 = open("26th-steps-videolinks.dat", "w", encoding = "utf-8")
-f4 = open("26th-steps-json.json", "w", encoding = "utf-8") # required for awards processing
+f4 = open("26th-steps-json.dat", "w", encoding = "utf-8") # required for awards processing
 awardJSONDATA = {}
-videoLISTDATA = []
 ## Processing data in JSON format
 for track in theJSON:
     # Going through each 'track' (courses/modules)
@@ -27,16 +25,12 @@ for track in theJSON:
         # Going through each 'project' in a 'track'
         projectNAME = project["name"]
         projectVIDEOURL = project["videoLink"]
-        if projectVIDEOURL:
-            videoLISTDATA.append(f"{trackCODE}-{project['refId']}: {projectVIDEOURL}\n")
         projectMEMBERS = list(map(lambda x: nameDCT[x], project["members"]))
         projectNUMBER = project["refId"]
         f2.write(f"{trackCODE}-{projectNUMBER}: {projectNAME} \n\n")
-        awardJSONDATA[f"{trackCODE}-{projectNUMBER}"] = {"name": projectNAME, "members": projectMEMBERS}
+        awardJSONDATA[f"{trackCODE}-{projectNUMBER}"] = {"name": projectNAME, "members": projectMEMBERS, "videoLink": projectVIDEOURL}
 
-f4.write(json.dumps(awardJSONDATA))
-f3.writelines(videoLISTDATA)
+f4.write(json.dumps(awardJSONDATA, indent = 4))
 ## Closing the files
 f2.close()
-f3.close()
 f4.close()
--- videodownload.py
+++ videodownload.py
@@ -5,6 +5,7 @@
 
 ## Importing the neccessary libraries
 from pytubefix import YouTube
+import json
 import ffmpeg
 import os
 
@@ -12,22 +13,33 @@ import os
 errorlog = open("errorlog.md", "w")
 errorlog.write(f"# Error Log\n\n")
 ## Downloading videos
-with open("26th-steps-videolinks.dat", "r") as f:
-    data = f.read().split("\n")
-    for entry in data:
-        entry = entry.split(": ")
+with open("26th-steps-json.dat", "r") as f:
+    data = json.load(f)
+    for courseName, data in data.items():
+        vLink = data["videoLink"]
+        if vLink == "":
+            print(f"{courseName} has no videos...")
+            continue
         try:
+            print(f"{courseName} has videos. Downloading...")
             # Downloading the video for the project in question
-            yt = YouTube(entry[1])
+            yt = YouTube(vLink)
             vStream = yt.streams.order_by('resolution').desc().first()
             aStream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
             vStream.download(filename="temp1a.webm")
             aStream.download(filename="temp2a.webm")
-            ffmpeg.output(ffmpeg.input("temp1a.webm"), ffmpeg.input("temp2a.webm"), f"videos/{entry[0]}.mp4").run()
+            ffmpeg.output(ffmpeg.input("temp1a.webm"), ffmpeg.input("temp2a.webm"), f"videos/{courseName}.mp4").run()
+            print(f"{courseName} video downloaded.")
         except:
             # Logging errors
-            errorlog.write(f"[{entry[0]}]({entry[1]})\n\n")
+            print(f"{courseName} video download failed. Writing to error log...")
+            errorlog.write(f"[{courseName}]({vLink})\n\n")
 ## Cleanup of temporary files
 errorlog.close()
-os.remove("temp1a.webm")
-os.remove("temp2a.webm")
+print("Videos have been downloaded.")
+try:
+    os.remove("temp1a.webm")
+    os.remove("temp2a.webm")
+    print("Temporary files removed.")
+except:
+    print("Nothing needs to be removed. Temporary files were removed.")
