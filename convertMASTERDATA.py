a5735d1b8de11232ef6a4b9402ef63a242c4f44a
zhd1987e <zhanghaodong101@outlook.com>
2025-03-30 19:00:59 +0800

    Bug in output file found

--- convertMASTERDATA.py
+++ convertMASTERDATA.py
@@ -3,7 +3,7 @@ import json
 
 jOUT = {}
 f1 = open('MASTERDATACSV.csv', 'r')
-f2 = open('masterDATA.dat', 'w')
+f2 = open('26th-steps-awardData.dat', 'w')
 csvreader = csv.reader(f1)
 for thing in list(csvreader):
     jOUT[thing[0]] = {"name": thing[1], "maxCerts": int(thing[2]), "isGraduate": thing[3] == "TRUE"}
